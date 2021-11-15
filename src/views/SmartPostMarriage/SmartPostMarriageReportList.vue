d5421dcdf369843b1aadc3bcda206bcf97c05a59
lord <lordywq@gmail.com>
2021-11-15 09:57:20 +0800

    婚后报备审核状态添加

--- src/views/SmartPostMarriage/SmartPostMarriageReportList.vue
+++ src/views/SmartPostMarriage/SmartPostMarriageReportList.vue
@@ -114,26 +114,17 @@
         </template>
 
         <span slot="action" slot-scope="text, record">
-          <!-- <a @click="handleEdit(record)">编辑</a> -->
+          <a v-show="record.verifyStatus == '3'" @click="handleEdit(record)">编辑</a>
 
-          <a-menu-item>
-            <a @click="handleDetail(record)">详情</a>
-          </a-menu-item>
+          <a-divider type="vertical" />
 
-          <!-- <a-divider type="vertical" /> -->
-          <!-- <a-dropdown>
-            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
-            <a-menu slot="overlay">
-              <a-menu-item>
-                <a @click="handleDetail(record)">详情</a>
-              </a-menu-item>
-              <a-menu-item>
-                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
-                  <a>删除</a>
-                </a-popconfirm>
-              </a-menu-item>
-            </a-menu>
-          </a-dropdown> -->
+          <a @click="handleDetail(record)">详情</a>
+
+          <a-divider type="vertical" />
+          
+          <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+            <a v-show="record.verifyStatus == '3'">删除</a>
+          </a-popconfirm>
         </span>
       </a-table>
     </div>
@@ -147,6 +138,7 @@ import { JeecgListMixin } from '@/mixins/JeecgListMixin'
 import SmartPostMarriageReportModal from './modules/SmartPostMarriageReportModal'
 import { filterMultiDictText } from '@/components/dict/JDictSelectUtil'
 import '@/assets/less/TableExpand.less'
+// import func from 'vue-editor-bridge'
 
 export default {
   name: 'SmartPostMarriageReportList',
@@ -273,11 +265,27 @@ export default {
           dataIndex: 'phoneNumber',
         },
         {
+          title: '审核状态',
+          align: 'center',
+          dataIndex: 'verifyStatus',
+          customRender: function (text) {
+            if (text == '0') {
+              return '不通过'
+            } else if (text == '1') {
+              return '通过'
+            } else if (text == '2') {
+              return '待审核'
+            } else {
+              return '免审'
+            }
+          },
+        },
+        {
           title: '操作',
           dataIndex: 'action',
           align: 'center',
           fixed: 'right',
-          width: 70,
+          width: 147,
           scopedSlots: { customRender: 'action' },
         },
       ],
