b7703287c75f3df6338c84d0fbe1aacc3d80eae4
MrYsL <1501010883@qq.com>
2021-12-01 21:09:58 +0800

    不同角色进入不同首页

--- src/components/tools/Breadcrumb.vue
+++ src/components/tools/Breadcrumb.vue
@@ -9,7 +9,9 @@
   </a-breadcrumb>
 </template>
 
+
 <script>
+import store from '@/store'
 export default {
     data() {
       return {
@@ -26,6 +28,31 @@ export default {
       console.log('this.$route.matched', this.$route.matched)
 
       this.breadList = []
+      // let flag = 0;
+      // for(let roleId of store.getters.role)
+      // {
+      //   if(roleId == "1463074308371800066"||roleId == "1463112478345588738")
+      //   {
+      //     this.flag = 2;
+      //   }
+      // }
+      // for(let roleId of store.getters.role)
+      // {
+      //   if(roleId == "f6817f48af4fb3af11b9e8bf182f618b")
+      //   {
+      //     this.flag = 1;
+      //   }
+      // }
+      // if(this.flag === 1)
+      // {
+      //   this.breadList.push({ name: 'dashboard-analysis', path: '/b5323bdac50f5bc855be3cf9c24f888a', meta: { title: '首页' } })
+      // }else if(this.flag === 2)
+      // {
+      //   this.breadList.push({ name: 'dashboard-analysis', path: '/interaction/home1', meta: { title: '首页' } })
+      // }
+      // else {
+      //   this.breadList.push({ name: 'dashboard-analysis', path: '/ok', meta: { title: '首页' } })
+      // }
       this.breadList.push({ name: 'dashboard-analysis', path: '/dashboard/analysis', meta: { title: '首页' } })
 
       this.name = this.$route.name
