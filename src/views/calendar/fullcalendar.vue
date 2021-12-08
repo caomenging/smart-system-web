9124a7296e58468403b74dc338de45b49c47a71a
XYY <machine.xyy@gmail.com>
2021-12-08 22:10:08 +0800

    修改日历组件,不同视图设置不同的高度,可在引用时指定.修改首页日历组件样式.

--- src/views/calendar/fullcalendar.vue
+++ src/views/calendar/fullcalendar.vue
@@ -1,43 +1,42 @@
 <template>
-  <div v-loading="loading">
-    <a-row style="margin: 10px 0px">
-      <a-col :span="24">
-        <a-button-group>
-          <a-button @click="calendarApi.prevYear()">
-            <a-icon type="double-left"/>
+  <div v-loading='loading' style='padding: 20px; background: white'>
+    <a-row style='margin: 10px 0px'>
+      <a-col>
+        <a-button-group style='margin-top: 10px'>
+          <a-button @click='calendarApi.prevYear()'>
+            <a-icon type='double-left' />
           </a-button>
-          <a-button @click="calendarApi.prev()">
-            <a-icon type="left"/>
+          <a-button @click='calendarApi.prev()'>
+            <a-icon type='left' />
           </a-button>
-          <a-button @click="calendarApi.today()"> Today</a-button>
-          <a-button @click="calendarApi.next()">
-            <a-icon type="right"/>
+          <a-button @click='calendarApi.today()'> Today</a-button>
+          <a-button @click='calendarApi.next()'>
+            <a-icon type='right' />
           </a-button>
-          <a-button @click="calendarApi.nextYear()">
-            <a-icon type="double-right"/>
+          <a-button @click='calendarApi.nextYear()'>
+            <a-icon type='double-right' />
           </a-button>
         </a-button-group>
-
-        <a-select :default-value="initialView" style="margin-left: 10px;width: 100px" @change="changeView">
-          <a-select-option v-for="(item, index) in views" :key="index" :value="item.value">
+        <a-select :default-value='initialView' style='width: 100px;margin-top: 10px;margin-left: 10px'
+                  @change='changeView'>
+          <a-select-option v-for='(item, index) in views' :key='index' :value='item.value'>
             {{ item.text }}
           </a-select-option>
         </a-select>
-
-        <a-button @click="handleAdd" type="primary" icon="plus" style="margin-left: 10px">新增</a-button>
+        <a-button @click='handleAdd' type='primary' icon='plus' style='margin-top: 10px;margin-left: 10px;'>新增</a-button>
       </a-col>
     </a-row>
 
     <FullCalendar
-      ref="fullCalendar"
+      ref='fullCalendar'
       :options='calendarOptions'
     >
       <template v-slot:eventContent='arg'>
         <b>{{ arg.timeText }}</b>
-        <i style="margin-left: 5px">{{ arg.event.title }}</i>
+        <i style='margin-left: 5px'>{{ arg.event.title }}</i>
       </template>
     </FullCalendar>
-    <calendar-event-modal ref="modalForm" @ok="modalFormOk"></calendar-event-modal>
+    <calendar-event-modal ref='modalForm' @ok='modalFormOk'></calendar-event-modal>
   </div>
 </template>
 
@@ -46,17 +45,17 @@ import '@fullcalendar/core/vdom' // solves problem with Vit
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import listPlugin from '@fullcalendar/list';
+import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
 
 import '@/assets/less/TableExpand.less'
-import {mixinDevice} from '@/utils/mixin'
-import {JeecgListMixin} from '@/mixins/JeecgListMixin'
+import { mixinDevice } from '@/utils/mixin'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
 import CalendarEventModal from './modules/CalendarEventModal'
-import {getAction, httpAction} from "../../api/manage";
+import { getAction, httpAction } from '../../api/manage'
 
 export default {
-  name: "fullcalendar",
+  name: 'fullcalendar',
   // mixins: [JeecgListMixin, mixinDevice],
   components: {
     CalendarEventModal,
@@ -77,6 +76,31 @@ export default {
       type: Number,
       required: false,
       default: 1
+    },
+    dayGridHeight: {
+      type: String | Number,
+      required: false,
+      default: null
+    },
+    timeGridHeight: {
+      type: String | Number,
+      required: false,
+      default: null
+    },
+    weekHeight: {
+      type: String | Number,
+      required: false,
+      default: null
+    },
+    dayHeight: {
+      type: String | Number,
+      required: false,
+      default: null
+    },
+    listHeight: {
+      type: String | Number,
+      required: false,
+      default: null
     }
   },
   data() {
@@ -106,8 +130,30 @@ export default {
         eventDrop: this.handleEventDrop,
         eventResize: this.handleEventDrop,
         eventsSet: this.handleEvents,
-        timeZone: "local",
-        firstDay: this.firstDay
+        timeZone: 'local',
+        firstDay: this.firstDay,
+        views: {
+          dayGrid: {
+            // options apply to dayGridMonth, dayGridWeek, and dayGridDay views
+            contentHeight: this.dayGridHeight
+          },
+          timeGrid: {
+            // options apply to timeGridWeek and timeGridDay views
+            contentHeight: this.timeGridHeight
+          },
+          week: {
+            // options apply to dayGridWeek and timeGridWeek views
+            contentHeight: this.weekHeight
+          },
+          day: {
+            // options apply to dayGridDay and timeGridDay views
+            contentHeight: this.dayHeight
+          },
+          list: {
+            // options apply to list views
+            contentHeight: this.listHeight
+          }
+        }
         // events: this.getData(this)
         /* you can update a remote database when these fire:
         eventAdd:
@@ -120,12 +166,12 @@ export default {
       loading: false,
 
       url: {
-        list: "/calendarEvent/calendarEvent/list",
-        edit: "/calendarEvent/calendarEvent/edit",
-        delete: "/calendarEvent/calendarEvent/delete",
-        deleteBatch: "/calendarEvent/calendarEvent/deleteBatch",
-        exportXlsUrl: "/calendarEvent/calendarEvent/exportXls",
-        importExcelUrl: "calendarEvent/calendarEvent/importExcel",
+        list: '/calendarEvent/calendarEvent/list',
+        edit: '/calendarEvent/calendarEvent/edit',
+        delete: '/calendarEvent/calendarEvent/delete',
+        deleteBatch: '/calendarEvent/calendarEvent/deleteBatch',
+        exportXlsUrl: '/calendarEvent/calendarEvent/exportXls',
+        importExcelUrl: 'calendarEvent/calendarEvent/importExcel'
       },
       views: [{
         text: '月',
@@ -147,7 +193,7 @@ export default {
         value: 'listDay'
       }],
       dictOptions: {},
-      superFieldList: [],
+      superFieldList: []
     }
   },
   mounted() {
@@ -156,6 +202,11 @@ export default {
     this.loadData()
   },
   methods: {
+    reRender() {
+      // 可以调用此函数重新绘制
+      this.calendarApi.render()
+    },
+
     changeView(type) {
       this.calendarApi.changeView(type)
     },
@@ -170,9 +221,9 @@ export default {
     },
 
     handleDateSelect(selectInfo) {
-      this.$refs.modalForm.add(selectInfo);
-      this.$refs.modalForm.title = "新增";
-      this.$refs.modalForm.disableSubmit = false;
+      this.$refs.modalForm.add(selectInfo)
+      this.$refs.modalForm.title = '新增'
+      this.$refs.modalForm.disableSubmit = false
     },
 
     handleEventClick(clickInfo) {
@@ -181,14 +232,14 @@ export default {
         id: event.id,
         title: event.title,
         eventDetail: event.extendedProps.eventDetail,
-        startTime: this.$moment(event.startStr).format("YYYY-MM-DD HH:mm:ss"),
+        startTime: this.$moment(event.startStr).format('YYYY-MM-DD HH:mm:ss'),
         backgroundColor: event.backgroundColor,
         borderColor: event.backgroundColor,
-        endTime: event.end ? this.$moment(event.endStr).format("YYYY-MM-DD HH:mm:ss") : "",
+        endTime: event.end ? this.$moment(event.endStr).format('YYYY-MM-DD HH:mm:ss') : '',
         allDay: event.allDay ? 1 : 0
-      });
-      this.$refs.modalForm.title = "编辑";
-      this.$refs.modalForm.disableSubmit = false;
+      })
+      this.$refs.modalForm.title = '编辑'
+      this.$refs.modalForm.disableSubmit = false
     },
 
     handleEventDrop(dropInfo) {
@@ -200,20 +251,20 @@ export default {
       // 后台自动修改
       let record = {
         id: event.id,
-        startTime: event.start ? that.$moment(event.startStr).format("YYYY-MM-DD HH:mm:ss") : null,
-        endTime: event.end ? that.$moment(event.endStr).format("YYYY-MM-DD HH:mm:ss") : null,
+        startTime: event.start ? that.$moment(event.startStr).format('YYYY-MM-DD HH:mm:ss') : null,
+        endTime: event.end ? that.$moment(event.endStr).format('YYYY-MM-DD HH:mm:ss') : null,
         allDay: event.allDay ? 1 : 0
       }
 
       httpAction(that.url.edit, record, 'put').then((res) => {
         if (res.success) {
-          that.$message.success(res.message);
+          that.$message.success(res.message)
         } else {
-          that.$message.warning(res.message);
+          that.$message.warning(res.message)
         }
       }).finally(() => {
-        that.loading = false;
-      });
+        that.loading = false
+      })
     },
 
     clearEvents() {
@@ -228,23 +279,23 @@ export default {
     },
 
     handleAdd() {
-      this.$refs.modalForm.add();
-      this.$refs.modalForm.title = "新增";
-      this.$refs.modalForm.disableSubmit = false;
+      this.$refs.modalForm.add()
+      this.$refs.modalForm.title = '新增'
+      this.$refs.modalForm.disableSubmit = false
     },
 
     loadData(arg) {
       if (!this.url.list) {
-        this.$message.error("请设置url.list属性!")
+        this.$message.error('请设置url.list属性!')
         return
       }
       this.loading = true
       //加载数据 若传入参数1则加载第一页的内容
       if (arg === 1) {
-        this.ipagination.current = 1;
+        this.ipagination.current = 1
       }
-      let params = {};//查询条件
-      this.loading = true;
+      let params = {}//查询条件
+      this.loading = true
       getAction(this.url.list, params).then((res) => {
         if (res.success) {
           for (let i = 0; i < res.result.records.length; i++) {
@@ -258,7 +309,7 @@ export default {
               backgroundColor: event.backgroundColor,
               end: event.endTime,
               allDay: event.allDay === 1
-            });
+            })
           }
         } else {
           this.$message.warning(res.message)
@@ -266,7 +317,7 @@ export default {
       }).finally(() => {
         this.loading = false
       })
-    },
+    }
   }
 }
 </script>
--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -3,7 +3,9 @@
     <index-chart v-if="indexStyle==1"></index-chart>
     <index-bdc v-if="indexStyle==2"></index-bdc>
     <index-task v-if="indexStyle==3"></index-task>
-    <fullcalendar :disabled="false" initialView="dayGridMonth"></fullcalendar>
+    <a-card title="办公日历" :bordered="false" style="margin-top: 20px">
+      <fullcalendar :disabled="false" initialView="dayGridMonth"></fullcalendar>
+    </a-card>
 <!--    <div style="width: 100%;text-align: right;margin-top: 20px">-->
 <!--      请选择首页样式：-->
 <!--      <a-radio-group v-model="indexStyle">-->
