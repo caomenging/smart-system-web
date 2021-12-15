17c62951557816dbb2c32a29507a25ae87c96811
wangxinran <haha1027874274@163.com>
2021-12-15 10:23:53 +0800

    '三会一课增加填报说明'

--- src/views/smartThreeMeetingOneLesson/modules/SmartThreeMeetingOneLessonModal.vue
+++ src/views/smartThreeMeetingOneLesson/modules/SmartThreeMeetingOneLessonModal.vue
@@ -8,6 +8,16 @@
     @ok="handleOk"
     :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
     @cancel="handleCancel">
+
+    <a-row type="flex" justify="center">
+      <a-col :span="22">
+        <a-card style="width: 100%; margin: 1rem 0">
+          <a-card-meta title="填表说明" :description="typeDesc">
+          </a-card-meta>
+        </a-card>
+      </a-col>
+    </a-row>
+
     <smart-three-meeting-one-lesson-form ref="realForm" @ok="submitCallback" :disabled="disableSubmit"/>
   </j-modal>
 </template>
@@ -15,6 +25,7 @@
 <script>
 
   import SmartThreeMeetingOneLessonForm from './SmartThreeMeetingOneLessonForm'
+  import {getAction} from "../../../api/manage";
 
   export default {
     name: 'SmartThreeMeetingOneLessonModal',
@@ -26,12 +37,22 @@
         title:'',
         width:800,
         visible: false,
-        disableSubmit: false
+        disableSubmit: false,
+
+        typeDesc: '',
+        typeName: '三会一课'
       }
     },
     methods:{
       add () {
         this.visible=true
+
+        getAction('/taskType/smartVerifyType/queryByTypeName' , {typeName: this.typeName}).then((res) => {
+          if(res.success){
+            this.typeDesc = res.result
+          }
+        })
+
         this.$nextTick(()=>{
           this.$refs.realForm.add();
         })
