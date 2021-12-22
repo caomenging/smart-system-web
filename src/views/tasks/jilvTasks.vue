9a5c8fa69c0713edbe554b4ae19e2c34464b7d65
sikex <yoloxz719@gmail.com>
2021-12-22 19:36:24 +0800

    feat: 添加纪律监督首页

---
+++ src/views/tasks/jilvTasks.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h2>监督检查任务列表</h2>
+  <jilv-tasks-list />
+  <h2>监督检查审核未通过列表</h2>
+  <jilv-not-pass-list />
+</div>
+</template>
+
+<script>
+import JilvNotPassList from './modules/jilvNotPassList.vue'
+import jilvTasksList from './modules/jilvTasksList.vue'
+export default {
+  components: { jilvTasksList, JilvNotPassList },
+
+}
+</script>
+
+<style>
+
+</style>
