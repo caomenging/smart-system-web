93a5895edeb9015a3dbab13bad3584edbd2d7e94
Purpose <machine.xyy@gmail.com>
2021-12-26 16:02:01 +0800

    添加宣传教育页面 (#112)

---
+++ src/views/PublicityEducation/modules/PublicityEducationForm.vue
@@ -0,0 +1,227 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <!-- 主表单区域 -->
+      <a-form-model ref="form" :model="model" :rules="validatorRules" slot="detail">
+        <a-row>
+          <a-col :span="24" >
+            <a-form-model-item label="村庄" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="village">
+              <a-input v-model="model.village" placeholder="请输入村庄" ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" >
+            <a-form-model-item label="标题" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="title">
+              <a-input v-model="model.title" placeholder="请输入标题" ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" >
+            <a-form-model-item label="地点" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="address">
+              <a-input v-model="model.address" placeholder="请输入地点" ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" >
+            <a-form-model-item label="时间" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="time">
+              <j-date placeholder="请选择时间" v-model="model.time" :show-time="true" date-format="YYYY-MM-DD HH:mm:ss" style="width: 100%" />
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </j-form-container>
+      <!-- 子表单区域 -->
+    <a-tabs v-model="activeKey" @change="handleChangeTabs">
+      <a-tab-pane tab="宣传教育参会人员" :key="refKeys[0]" :forceRender="true">
+        <j-editable-table
+          :ref="refKeys[0]"
+          :loading="publicityEducationPacpaTable.loading"
+          :columns="publicityEducationPacpaTable.columns"
+          :dataSource="publicityEducationPacpaTable.dataSource"
+          :maxHeight="300"
+          :disabled="formDisabled"
+          :rowNumber="true"
+          :rowSelection="true"
+          :actionButton="true"/>
+      </a-tab-pane>
+      <a-tab-pane tab="宣传教育附件表" :key="refKeys[1]" :forceRender="true">
+        <j-editable-table
+          :ref="refKeys[1]"
+          :loading="publicityEducationAnnexTable.loading"
+          :columns="publicityEducationAnnexTable.columns"
+          :dataSource="publicityEducationAnnexTable.dataSource"
+          :maxHeight="300"
+          :disabled="formDisabled"
+          :rowNumber="true"
+          :rowSelection="true"
+          :actionButton="true"/>
+      </a-tab-pane>
+    </a-tabs>
+  </a-spin>
+</template>
+
+<script>
+
+  import { getAction } from '@/api/manage'
+  import { FormTypes,getRefPromise,VALIDATE_NO_PASSED } from '@/utils/JEditableTableUtil'
+  import { JEditableTableModelMixin } from '@/mixins/JEditableTableModelMixin'
+  import { validateDuplicateValue } from '@/utils/util'
+
+  export default {
+    name: 'PublicityEducationForm',
+    mixins: [JEditableTableModelMixin],
+    components: {
+    },
+    data() {
+      return {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 6 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        labelCol2: {
+          xs: { span: 24 },
+          sm: { span: 3 },
+        },
+        wrapperCol2: {
+          xs: { span: 24 },
+          sm: { span: 20 },
+        },
+        model:{
+        },
+        // 新增时子表默认添加几行空数据
+        addDefaultRowNum: 1,
+        validatorRules: {
+           village: [
+              { required: true, message: '请输入村庄!'},
+           ],
+           title: [
+              { required: true, message: '请输入标题!'},
+           ],
+           address: [
+              { required: true, message: '请输入地点!'},
+           ],
+           time: [
+              { required: true, message: '请输入时间!'},
+           ],
+        },
+        refKeys: ['publicityEducationPacpa', 'publicityEducationAnnex', ],
+        tableKeys:['publicityEducationPacpa', 'publicityEducationAnnex', ],
+        activeKey: 'publicityEducationPacpa',
+        // 宣传教育参会人员
+        publicityEducationPacpaTable: {
+          loading: false,
+          dataSource: [],
+          columns: [
+            {
+              title: '参会人员姓名',
+              key: 'name',
+              type: FormTypes.input,
+              width:"200px",
+              placeholder: '请输入${title}',
+              defaultValue:'',
+              validateRules: [{ required: true, message: '${title}不能为空' }],
+            },
+          ]
+        },
+        // 宣传教育附件表
+        publicityEducationAnnexTable: {
+          loading: false,
+          dataSource: [],
+          columns: [
+            {
+              title: '附件文件',
+              key: 'filepath',
+              type: FormTypes.file,
+              token:true,
+              responseName:"message",
+              width:"200px",
+              placeholder: '请选择文件',
+              defaultValue:'',
+              validateRules: [{ required: true, message: '${title}不能为空' }],
+            },
+          ]
+        },
+        url: {
+          add: "/publicityEducation/publicityEducation/add",
+          edit: "/publicityEducation/publicityEducation/edit",
+          queryById: "/publicityEducation/publicityEducation/queryById",
+          publicityEducationPacpa: {
+            list: '/publicityEducation/publicityEducation/queryPublicityEducationPacpaByMainId'
+          },
+          publicityEducationAnnex: {
+            list: '/publicityEducation/publicityEducation/queryPublicityEducationAnnexByMainId'
+          },
+        }
+      }
+    },
+    props: {
+      //表单禁用
+      disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    computed: {
+      formDisabled(){
+        return this.disabled
+      },
+    },
+    created () {
+    },
+    methods: {
+      addBefore(){
+        this.publicityEducationPacpaTable.dataSource=[]
+        this.publicityEducationAnnexTable.dataSource=[]
+      },
+      getAllTable() {
+        let values = this.tableKeys.map(key => getRefPromise(this, key))
+        return Promise.all(values)
+      },
+      /** 调用完edit()方法之后会自动调用此方法 */
+      editAfter() {
+        this.$nextTick(() => {
+        })
+        // 加载子表数据
+        if (this.model.id) {
+          let params = { id: this.model.id }
+          this.requestSubTableData(this.url.publicityEducationPacpa.list, params, this.publicityEducationPacpaTable)
+          this.requestSubTableData(this.url.publicityEducationAnnex.list, params, this.publicityEducationAnnexTable)
+        }
+      },
+      //校验所有一对一子表表单
+      validateSubForm(allValues){
+          return new Promise((resolve,reject)=>{
+            Promise.all([
+            ]).then(() => {
+              resolve(allValues)
+            }).catch(e => {
+              if (e.error === VALIDATE_NO_PASSED) {
+                // 如果有未通过表单验证的子表，就自动跳转到它所在的tab
+                this.activeKey = e.index == null ? this.activeKey : this.refKeys[e.index]
+              } else {
+                console.error(e)
+              }
+            })
+          })
+      },
+      /** 整理成formData */
+      classifyIntoFormData(allValues) {
+        let main = Object.assign(this.model, allValues.formValue)
+        return {
+          ...main, // 展开
+          publicityEducationPacpaList: allValues.tablesValue[0].values,
+          publicityEducationAnnexList: allValues.tablesValue[1].values,
+        }
+      },
+      validateError(msg){
+        this.$message.error(msg)
+      },
+
+    }
+  }
+</script>
+
+<style scoped>
+</style>
