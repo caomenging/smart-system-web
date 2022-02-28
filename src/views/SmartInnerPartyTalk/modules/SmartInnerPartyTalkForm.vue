de24b3f653c9694e7fef04bc1bd76f0d818c7177
ZXH <51884587+ZXH-hub@users.noreply.github.com>
2022-02-28 12:39:17 +0800

    党内谈话高拍仪修改 (#148)
    * 三员+调查问卷
    
    * 三重一大首页审核列表&三员＋缓存优化
    
    * 三员＋列表缓存优化
    
    * 评廉&三员＋修改
    
    * 评廉列表路径修改
    
    * 三员＋修改&页面跳转
    
    * commit
    
    * 三员+路由跳转优化
    
    * 三重一大首页&其他微调
    
    * 人员、评廉修改1.0
    
    * 人员、评廉修改1.0
    
    * 党内谈话添加高拍仪
    
    * 党内谈话高拍仪修改
    
    Co-authored-by: Riley <yoloxz719@gmail.com>

--- src/views/SmartInnerPartyTalk/modules/SmartInnerPartyTalkForm.vue
+++ src/views/SmartInnerPartyTalk/modules/SmartInnerPartyTalkForm.vue
@@ -77,7 +77,7 @@
           <a-col :span="24" >
             <a-form-model-item label="附件" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="files">
               <a-button icon="camera" @click="eloamScan">高拍仪拍照</a-button>
-              <j-upload v-model="model.files" ></j-upload>
+<!--              <j-upload v-model="model.files" ></j-upload>-->
             </a-form-model-item>
           </a-col>
         </a-row>
@@ -260,14 +260,16 @@
       },
       scanOk(url) {
         let image = url
+        // 请根据自己表单中的字段名称修改 field 变量的值
+        let field = 'files'
         if (image) {
           let arr = []
-          if (this.model.files) {
-            arr.push(this.model.files)
+          // 考虑如果存在已经上传的文件，则拼接起来，没有则直接添加
+          if (this.model[field]) {
+            arr.push(this.model[field])
           }
           arr.push(image)
-          // 更新表单中文件url字段, files 为字段名称
-          this.$set(this.model, 'files', arr.join())
+          this.$set(this.model, field, arr.join())
         }
       },
       addBefore(){
